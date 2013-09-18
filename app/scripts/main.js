12826f80d2ab28ea51b2dde4df3e71533014fcd9
Josh Walsh <jwalsh@designinginteractive.com>
2013-09-18 10:44:49 -0400

    Add dummy routes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,6 @@
 App = Ember.Application.create({
 });
+
+App.Router.map(function() {
+  this.route('help');
+});
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,9 +1,43 @@
+Ember.TEMPLATES["help"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("Back");
+  }
+
+  data.buffer.push("<h1>Prototype Help</h1>\n<p>For help building your prototype, see the README.</p>\n\n");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n");
+  return buffer;
+  
+});
+
 Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
   
+  
+  data.buffer.push("Getting Started");
+  }
 
-
-  data.buffer.push("<h1>Prototype Home Page</h1>\n<p>Find me in app/templates/index.handlebars</p>\n");
+  data.buffer.push("<h1>Prototype Home Page</h1>\n<p>Find me in app/templates/index.handlebars</p>\n\n");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "help", options) : helperMissing.call(depth0, "link-to", "help", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n");
+  return buffer;
   
 });
